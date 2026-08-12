2f825d99a7905ad1e776f5b2bbda8fe98dbeee83
Tomaz Stih <tomaz.stih@wischner.co.uk>
2026-08-12 02:04:50 +0100

    Added Loading... to start up.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <partner/conio.h>
 
 #include <animate.h>
+
+static const char loading_text[]="Loading...";
 
 /* Load the whole file into a freshly allocated buffer. */
 static uint8_t *fload(const char *path) {
@@ -27,30 +30,48 @@ static uint8_t *fload(const char *path) {
 }
 
 int main(int argc, char *argv[]) {
+    struct text_info screen;
+    uint8_t *ani;
+    int loading_x;
+
+    setcursortype(NOCURSOR);
+    clrscr();
 
     /* must have exactly 1 argument */
     if (argc!=2) {
+        setcursortype(NORMALCURSOR);
         printf("Invalid command line arguments.\n");
         printf("Usage: animate <path>\n");
         return 1;
     }
 
     /* load the file */
-    uint8_t *ani=fload(argv[1]);
+    gettextinfo(&screen);
+    loading_x=((int)screen.screenwidth-(int)(sizeof(loading_text)-1))/2;
+    gotoxy(loading_x,screen.screenheight/2);
+    cputs(loading_text);
+
+    ani=fload(argv[1]);
     if (ani==NULL) {
+        clrscr();
+        setcursortype(NORMALCURSOR);
         printf("File %s could not be read.\n",argv[1]);
         return 2;
     }
 
     /* recognize file format */
     if (ani[0]!='A'||ani[1]!='N') {
+        clrscr();
+        setcursortype(NORMALCURSOR);
         printf("File %s does not look like an animation file.\n",argv[1]);
         free(ani);
         return 3;
     }
 
     /* and, finally, animate... */
+    clrscr();
     animate((animation_t *)ani);
+    setcursortype(NORMALCURSOR);
 
     /* release the file */
     free(ani);
